d04f9fb9d5c7e357961be9a7bf7b1f520ea78e1a
Alessandro <alessandrobuday@gmail.com>
2024-07-19 22:59:54 -0400

    Updated EasyItem. You can now set a custom NBT tag using the setNBT method.

--- src/main/java/it/vulpinefriend87/easyutils/EasyItem.java
+++ src/main/java/it/vulpinefriend87/easyutils/EasyItem.java
@@ -1,6 +1,9 @@
 package it.vulpinefriend87.easyutils;
 
+import net.minecraft.server.v1_12_R1.NBTBase;
+import net.minecraft.server.v1_12_R1.NBTTagCompound;
 import org.bukkit.Material;
+import org.bukkit.craftbukkit.v1_12_R1.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -8,7 +11,7 @@ import java.util.List;
 
 public class EasyItem {
 
-    private final ItemStack itemStack;
+    private ItemStack itemStack;
 
     public EasyItem(Material material, String displayName, List<String> lore) {
 
@@ -34,6 +37,48 @@ public class EasyItem {
             itemStack.setItemMeta(meta);
 
         }
+
+    }
+
+    public void setNBT(String key, Boolean value) {
+
+        net.minecraft.server.v1_12_R1.ItemStack NMSItemStack = CraftItemStack.asNMSCopy(itemStack);
+        NBTTagCompound tag = NMSItemStack.hasTag() ? NMSItemStack.getTag() : new NBTTagCompound();
+
+        assert tag != null;
+        tag.setBoolean(key, value);
+
+        NMSItemStack.setTag(tag);
+
+        itemStack = CraftItemStack.asBukkitCopy(NMSItemStack);
+
+    }
+
+    public void setNBT(String key, String value) {
+
+        net.minecraft.server.v1_12_R1.ItemStack NMSItemStack = CraftItemStack.asNMSCopy(itemStack);
+        NBTTagCompound tag = NMSItemStack.hasTag() ? NMSItemStack.getTag() : new NBTTagCompound();
+
+        assert tag != null;
+        tag.setString(key, value);
+
+        NMSItemStack.setTag(tag);
+
+        itemStack = CraftItemStack.asBukkitCopy(NMSItemStack);
+
+    }
+
+    public void setNBT(String key, Float value) {
+
+        net.minecraft.server.v1_12_R1.ItemStack NMSItemStack = CraftItemStack.asNMSCopy(itemStack);
+        NBTTagCompound tag = NMSItemStack.hasTag() ? NMSItemStack.getTag() : new NBTTagCompound();
+
+        assert tag != null;
+        tag.setFloat(key, value);
+
+        NMSItemStack.setTag(tag);
+
+        itemStack = CraftItemStack.asBukkitCopy(NMSItemStack);
 
     }
 
