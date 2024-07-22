282d3c888399ad62699d2a0cfc2748c03e458821
Alessandro <alessandrobuday@gmail.com>
2024-07-22 12:38:55 -0400

    Added EasyMessage

---
+++ src/main/java/it/vulpinefriend87/easyutils/EasyMessage.java
@@ -0,0 +1,28 @@
+package it.vulpinefriend87.easyutils;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+
+public class EasyMessage {
+    private TextComponent textComponent;
+
+    public EasyMessage(String message) {
+        textComponent = new TextComponent(Colorize.color(message));
+    }
+
+    public void setClickable(ClickEvent.Action action, String value) {
+        ClickEvent clickEvent = new ClickEvent(action, Colorize.color(value));
+        textComponent.setClickEvent(clickEvent);
+    }
+
+    public void setHoverable(HoverEvent.Action action, String value) {
+        TextComponent hoverText = new TextComponent(value);
+        HoverEvent hoverEvent = new HoverEvent(action, new net.md_5.bungee.api.chat.hover.content.Text(hoverText));
+        textComponent.setHoverEvent(hoverEvent);
+    }
+
+    public TextComponent getTextComponent() {
+        return textComponent;
+    }
+}
