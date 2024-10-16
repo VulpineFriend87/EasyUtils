9f0764a6e8d3545463f16bf88b0e36adb1727f75
Alessandro Buday <alessandrobuday@gmail.com>
2024-10-16 11:32:59 +0200

    Implemented Hex Color Codes in Colorize.

--- src/main/java/it/vulpinefriend87/easyutils/Colorize.java
+++ src/main/java/it/vulpinefriend87/easyutils/Colorize.java
@@ -1,13 +1,34 @@
 package it.vulpinefriend87.easyutils;
 
-import org.bukkit.ChatColor;
+import net.md_5.bungee.api.ChatColor;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Colorize {
 
-    public static String color(String stringToColor) {
+    private static final Pattern HEX_PATTERN = Pattern.compile("&#([A-Fa-f0-9]{6})");
 
-        return ChatColor.translateAlternateColorCodes('&', stringToColor);
+    public static String color(final String message) {
+        return translateHexColorCodes(ChatColor.translateAlternateColorCodes('&', message));
+    }
 
+    public static String translateHexColorCodes(final String message) {
+        final char colorChar = ChatColor.COLOR_CHAR;
+
+        final Matcher matcher = HEX_PATTERN.matcher(message);
+        final StringBuffer buffer = new StringBuffer(message.length() + 4 * 8);
+
+        while (matcher.find()) {
+            final String group = matcher.group(1);
+
+            matcher.appendReplacement(buffer, colorChar + "x"
+                    + colorChar + group.charAt(0) + colorChar + group.charAt(1)
+                    + colorChar + group.charAt(2) + colorChar + group.charAt(3)
+                    + colorChar + group.charAt(4) + colorChar + group.charAt(5));
+        }
+
+        return matcher.appendTail(buffer).toString();
     }
 
 }
